17d26799745ccf725bb34f6765b5a2e073a0e886
Tom <tombap8@hotmail.com>
2021-06-08 14:37:34 +0900

    Update main.js

--- 003.JS학습/shoppingMall_PJ/js/main.js
+++ 003.JS학습/shoppingMall_PJ/js/main.js
@@ -7,10 +7,22 @@ window.addEventListener("load",function(){
     // 1. 로드구역 실행확인
     console.log("로딩완료!");
 
-    // 2. 슬라이드 오른쪽 이동버튼 셋업
+    // 2. 변경대상: #slide
+    var slide = document.querySelector("#slide");
+
+    // 3. 슬라이드 오른쪽 이동버튼 셋업
     document.querySelector(".ab2").onclick = 
     function(){
+        // 1. 호출확인
         console.log("나,오른쪽!");
+        
+        // 2. 변경대상 : #slide -> 바깥에 변수 slide
+
+        // 3. 변경내용 : 슬라이드 left값 왼쪽이동으로 변경
+        slide.style.left = "-100%";
+        slide.style.transition = "left .8s ease-in-out";
+
+
     };/////// click이벤트 함수 ///////////////
 
 
