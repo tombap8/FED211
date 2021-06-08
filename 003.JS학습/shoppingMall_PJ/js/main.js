ba6f279a5370d76c687fd47d66209117bef25334
Tom <tombap8@hotmail.com>
2021-06-08 13:19:36 +0900

    ss

---
+++ 003.JS학습/shoppingMall_PJ/js/main.js
@@ -0,0 +1,19 @@
+// 쇼핑몰 PJ 메인 JS - main.js
+
+/////////// html로드구역 /////////////////////
+// load 이벤트 / DOMContentLoaded 이벤트
+window.addEventListener("load",function(){
+
+    // 1. 로드구역 실행확인
+    console.log("로딩완료!");
+
+    // 2. 슬라이드 오른쪽 이동버튼 셋업
+    document.querySelector(".ab2").onclick = 
+    function(){
+        console.log("나,오른쪽!");
+    };/////// click이벤트 함수 ///////////////
+
+
+
+
+});////////// 로드구역 ////////////////////////
