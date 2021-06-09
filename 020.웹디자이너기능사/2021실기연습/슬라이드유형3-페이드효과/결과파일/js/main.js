10a43f5cbbfcb9bf6278a340842678d2228fdf25
Tom <tombap8@hotmail.com>
2021-06-09 13:12:35 +0900

    ss

--- 020.웹디자이너기능사/2021실기연습/슬라이드유형3-페이드효과/결과파일/js/main.js
+++ 020.웹디자이너기능사/2021실기연습/슬라이드유형3-페이드효과/결과파일/js/main.js
@@ -7,40 +7,31 @@ console.log("연결확인!!!");
 
 /// 일정시간간격 호출하기 ///
 // setInterval(함수,시간) -> 시간은 1/1000초
+// 페이드 효과 넘기기
+
+// 슬라이드 순번변수
+var snum = 0;
+
 setInterval(function(){
 
+    // 0. 슬라이드 순번 증가하기!
+    snum++;
+    if(snum===3) snum=0;
+    //한계수에서 처음으로!
+
     // 1. 호출여부확인
-    console.log("인터발!!!");
+    console.log("인터발!!!"+snum);
 
-    // 2. 대상선정: .slide
-    var tg = document.querySelector(".slide");
+    // 2. 대상선정: .slide li
+    var tg = document.querySelectorAll(".slide li");
 
-    // 3. 변경내용: 대상의 top값이 -100%로 변경+트랜지션
-    tg.style.top = "-100%";
-    tg.style.transition = ".6s";
+    // 3. 대상모두 class="on" 제거하기
+    for(var i=0; i<3; i++){
+        tg[i].classList.remove("on");
+    } //////// for //////////////////
 
-    // 4. 이동후 작업! /////////////////////
-    // 위의 이동설정후에 적용해야함!(이동시간 0.6초후!)
-    // 왜? top값, transition값이 변경되므로!
-
-    // 0.6초후 한번 함수실행!//////////////
-    setTimeout(function(){
-
-        // 4-1. 첫번째 li를 잘라서 맨뒤로 이동함!
-        // 첫번째 li 선택
-        var first = tg.querySelectorAll("li")[0];
-        // 맨뒤로 이동 메서드 -> appendChild(요소)
-        tg.appendChild(first);
-    
-        // 4-2. top값이 -100%이므로 0으로 변경!
-        tg.style.top = "0";
-    
-        // 4-3. 이때 트랜지션 없애기!
-        tg.style.transition = "none";
-
-    },600);///-> 600은 0.6초 타임아웃함수 //////
-    ///////////////////////////////////////////
-
+    // 4. 해당순번만 class="on" 주기!
+    tg[snum].classList.add("on");
 
 },3000);// 인터발함수 //////////////
 ///////////////////////////////////
