311141c86e82b83fd256759c2d0bca1a41c4f5ec
Tom <tombap8@hotmail.com>
2021-06-16 16:05:01 +0900

    Update main.js

--- 001.HTML학습/022.CubeTrip/js/main.js
+++ 001.HTML학습/022.CubeTrip/js/main.js
@@ -10,6 +10,13 @@ $(function () { // jQB //////////////////////////////////
     // 대상: .city a
     $(".city a").click(function () {
 
+        // 0. 도시정보박스 숨기기(트랜지션없앨것!)
+        $(".cbx").css({
+            opacity:"0",
+            transition:"none"
+        });///////// css /////////
+
+
         // 1. a요소의 글자 읽어오기
         //  (this는 클릭된 a자신!)
         var txt = $(this).text();
@@ -43,6 +50,21 @@ $(function () { // jQB //////////////////////////////////
             transform: setval,
             transition: "1.5s ease-in-out"
         }); /////////// css /////////////////
+
+        // 4. 도시정보 셋팅하기 ///
+
+        // 5. 도시정보박스 보이기 ///
+        // 대상: .cbx
+        // 내용: 큐브가 1.5초 회전한 후 도시정보박스가 보인다
+        // 1.5초후에 실행하려면? setTimeout(함수,시간)
+        setTimeout(function(){
+            $(".cbx").css({
+                opacity:"1",
+                transition:"opacity .8s ease-in-out"
+            });///////////////// css /////////////////
+        },1500);////타임아웃함수 ///////////////////////
+
+
 
 
 
