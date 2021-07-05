f1376966984fc41b69f9a5b5a5fe77bf31cc450b
Tom <tombap8@hotmail.com>
2021-07-05 16:38:27 +0900

    Update linksys.js

--- 001.HTML학습/010.MyItem_PJ/02.Asset/js/linksys.js
+++ 001.HTML학습/010.MyItem_PJ/02.Asset/js/linksys.js
@@ -19,6 +19,26 @@ $(function () { //////// jQB /////////////////
         let txt = $(this).text();
         console.log("메뉴명: " + txt);
 
+        // 2. 링크분기하기
+
+        // 이동 url
+        let url;
+
+        switch (txt) {
+            case "Home":
+                url = "index.html";
+                break;
+            case "프로필":
+                url = "profile.html";
+                break;
+                // "Home"과 "프로필" 외에는 모두 아이템 페이지임!
+            default:
+                url = "item.html?itm=" + txt;
+        } ////////// switch case문 //////////////
+
+        // 분기 셋팅된 url확인!
+        console.log("url : " + url);
+
     }); /////////// click //////////////////
 
 
