9320a0bb426f63114806016593bdd5cb85615db2
Tom <tombap8@hotmail.com>
2021-07-07 12:11:39 +0900

    Update jquery.lightbox-0.5.js

--- 004.jQuery학습/jQuery-02.실전연습/01.jQuery_gallery/04_LightBox/js/jquery.lightbox-0.5.js
+++ 004.jQuery학습/jQuery-02.실전연습/01.jQuery_gallery/04_LightBox/js/jquery.lightbox-0.5.js
@@ -28,7 +28,9 @@
 			fixedNavigation:		false,		// (boolean) Boolean that informs if the navigation (next and prev button) will be fixed or not in the interface.
 			// Configuration related to images
 			imageLoading:			'images/lightbox-ico-loading.gif',		// (string) Path and the name of the loading icon
+			//imageBtnPrev:			'images/lightbox-btn-prev.gif',			// (string) Path and the name of the prev button image
 			imageBtnPrev:			'images/prev.png',			// (string) Path and the name of the prev button image
+			//imageBtnNext:			'images/lightbox-btn-prev.gif',			// (string) Path and the name of the next button image
 			imageBtnNext:			'images/next.png',			// (string) Path and the name of the next button image
 			imageBtnClose:			'images/lightbox-btn-close.gif',		// (string) Path and the name of the close btn
 			imageBlank:				'images/lightbox-blank.gif',			// (string) Path and the name of a blank image (one pixel)
