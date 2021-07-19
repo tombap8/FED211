79ae0852ab23930c80bc45be28c2aee6452835c5
Tom <tombap8@hotmail.com>
2021-07-19 13:18:20 +0900

    xx
    ss

--- 006.파일럿PJ/002.구현소스/js/main.js
+++ 006.파일럿PJ/002.구현소스/js/main.js
@@ -24,17 +24,27 @@ $(function () { /// jQB ////////////////////////
         // 0. 클릭된 li순번 구해오기
         let idx = $(this).index();
         console.log("순번:" + idx);
+        
 
         // 1. 하위a요소의 href값 읽어오기
-        let idnm = $("a", this).attr("href");
-        console.log("href값:" + idnm);
+        // let idnm = $("a", this).attr("href");
+        // console.log("href값:" + idnm);
 
         // 2. 아이디값에 해당하위 top값 위치구하기
         // top값을 구해서 스크롤 애니메이션 이동에 사용함!
-        let pos = $(idnm).offset().top;
+        // let pos = $(idnm).offset().top;
         // offset() 메서드 : 요소의 기본 셋팅 정보를 리턴함
         // - top,left,width,height 등 을 알수 있다!
+        
+        // 아이디요소의 위치값을 구해서 이동하면 되지만
+        // 최신 제이쿼리 라이브러리가 위치값을 잘못 구해오는
+        // 버그가 있으므로 페이지 높이를 기준해서 위치이동을 한다!
+        
+        ////////////////////////////////////////////////////
+        // 새로운 페이지 위치값 : 윈도우 높이값 * 페이지순번 //
+        let pos = $(window).height() * idx;
         console.log("위치값:" + pos);
+        ///////////////////////////////////////////////////
 
         // 3. 스크롤 애니메이션으로 페이지 이동하기
         // 세로스크롤 이동속성: scrollTop
@@ -45,15 +55,14 @@ $(function () { /// jQB ////////////////////////
             scrollTop: pos + "px"
         }, 1200, "easeOutQuint"); //// animate /////
 
-        // 4. 클릭된 a요소의 부모 li에 class="on" 넣기
+        // 4. 클릭된 li요소에 class="on" 넣기
         $(".gnb li").eq(idx).addClass("on")
             .siblings().removeClass("on");
         $(".indic li").eq(idx).addClass("on")
             .siblings().removeClass("on");
         //다른 형제 li들 class="on" 지움
 
-        // 6. li순번과 pno와 일치하기!
-        // 전역변수 페이지번호(pno)와 gnb메뉴 li순번과 같다!
+        // 6. li순번과 pno와 일치하기! /////////////////////
         pno = idx;
         console.log("페이지번호:" + pno);
 
