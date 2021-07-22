ebdc0e2c658fef31a242bbb3392cd23d7a96a4b4
Tom <tombap8@hotmail.com>
2021-07-22 13:12:21 +0900

    ss

--- 001.HTML학습/021.VOGUE_PJ/002.site/js/linksys.js
+++ 001.HTML학습/021.VOGUE_PJ/002.site/js/linksys.js
@@ -2,13 +2,23 @@
 
 $(function(){ //////// jQB ///////////////////////
 
+    // 로고 클릭시 메인 페이지로 이동하기
+    $(".logo a").click(function(e){
+        
+        // 기본이동막기
+        e.preventDefault();
+
+        location.href = "index.html";
+
+    });////////// click /////////////
+
     // GNB 메뉴 링크 연결하기 //
     // 대상: .gnb a
     $(".gnb a").click(function(e){
 
         // 기본이동막기
         e.preventDefault();
-        
+
         console.log("이동!");
 
     }); ///////// click //////////////
