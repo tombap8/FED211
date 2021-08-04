73989fc525e92fccd4d059387da81ee4f3bdfab6
Tom <tombap8@hotmail.com>
2021-08-04 15:49:48 +0900

    ss

--- 006.파일럿PJ/002.구현소스/js/men.js
+++ 006.파일럿PJ/002.구현소스/js/men.js
@@ -1,5 +1,26 @@
 // 남자패션페이지 JS - men.js
 
-$(function(){ /// jQB ////////////////////////////
+$(function () { /// jQB ////////////////////////////
+
+    /// 부드러운 스크롤 호출!(제이쿼리 아님!)
+    startSS();
+
+    /////////////////////////////////////
+    // 배너에 스와이퍼 플러그인 적용하기 /// 
+    /////////////////////////////////////
+    
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      }); //////// swiper ///////////////
 
 }); ///////////// jQB ////////////////////////////
