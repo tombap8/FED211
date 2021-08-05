d62eae92cf8a2f2494bc324e9f50d3f8f6232fd7
Tom <tombap8@hotmail.com>
2021-08-05 16:45:39 +0900

    Update main.js

--- 004.jQuery학습/jQuery-03.응용구현/Gurumi_PJ/js/main.js
+++ 004.jQuery학습/jQuery-03.응용구현/Gurumi_PJ/js/main.js
@@ -6,7 +6,7 @@ $(function () { //////////// jQB //////////////////////////
     let gbx = $(".gbx");
 
     // 광클금지변수
-    let prot = 0;// 1-불허용, 0-허용
+    let prot = 0; // 1-불허용, 0-허용
 
     // 오른쪽 이동버튼 기능을 함수로 만들고 호출
     // 왜? 자동호출도 같은 기능이므로!!!
@@ -19,11 +19,14 @@ $(function () { //////////// jQB //////////////////////////
     $(".rb").click(function () {
 
         // 광클금지 //////
-        if(prot) return;
+        if (prot) return;
         prot = 1;
         setTimeout(() => {
-            prot = 0;//해제
+            prot = 0; //해제
         }, 400);
+
+        // 자동호출 지우기
+        clearAuto();
 
         // 이동함수 호출
         goSlide();
@@ -33,12 +36,15 @@ $(function () { //////////// jQB //////////////////////////
     $(".lb").click(function () {
         
         // 광클금지 //////
-        if(prot) return;
+        if (prot) return;
         prot = 1;
         setTimeout(() => {
-            prot = 0;//해제
+            prot = 0; //해제
         }, 400);
 
+        // 자동호출 지우기
+        clearAuto();
+        
         // 마지막 요소를 잘라서 맨앞으로 보냄
         gbx.prepend(gbx.find("img").last());
     }); ///////////// click ////////////////
@@ -53,6 +59,19 @@ $(function () { //////////// jQB //////////////////////////
 
     // 자동 인터발함수 최초호출!
     autoSlide();
+
+    // 타임아웃용변수
+    let autoT;
+
+    ///// 자동인터발 지우기 함수 //////////
+    let clearAuto = () => {
+        // 인터발지우기
+        clearInterval(autoI);
+        // 타임아웃지우기(쓰나미실행방지)
+        clearTimeout(autoT);
+        // 타임아웃 설정(일정시간뒤 자동재실행)
+        autoT = setTimeout(autoSlide, 5000);
+    }; //////////// clearAuto 함수 ///////////
 
 
 
