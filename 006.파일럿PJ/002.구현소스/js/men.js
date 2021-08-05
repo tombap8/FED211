9c9d41f77d6d4bc1da53de03ea33180d972ee161
Tom <tombap8@hotmail.com>
2021-08-05 11:38:54 +0900

    ss

--- 006.파일럿PJ/002.구현소스/js/men.js
+++ 006.파일럿PJ/002.구현소스/js/men.js
@@ -2,80 +2,94 @@
 
 $(function () { /// jQB ////////////////////////////
 
-    /// 부드러운 스크롤 호출!(제이쿼리 아님!)
-    startSS();
+  /// 부드러운 스크롤 호출!(제이쿼리 아님!)
+  startSS();
 
-    /////////////////////////////////////
-    // 배너에 스와이퍼 플러그인 적용하기 /// 
-    /////////////////////////////////////
-    
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        loop: true,
-        speed: 500,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-          // disableOnInteraction 
-          // - 건드린 후 멈춤 true / 다시 작동은 false
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      }); //////// swiper ///////////////
+  /////////////////////////////////////
+  // 배너에 스와이퍼 플러그인 적용하기 /// 
+  /////////////////////////////////////
 
-
-      /// 스크롤 등장 플러그인 적용 : 스크롤리빌 ////
-      $.fn.scrollReveal();  
-
-      ////////////////////////////////////////////////
-      //////////////// 신상품 움직이기 ////////////////
-      ////////////////////////////////////////////////
-      // 움직일 대상: .flist
-      let flist = $(".flist");
-      // 위치값변수
-      let lpos = 0;
-
-      // 신상품 움직이기 함수 ////////////////
-      let moveList = () => {
-
-        // 1. 위치값 감소하기 /////
-        lpos--;
-        console.log("위치값:"+lpos);
-
-        // 2. 위치값 적용하기 //////
-        flist.css({
-          left: lpos + "px"
-        }); ////// css ////////////
-
-        // 3. 한계값 초기화하기 
-        //    + 첫번째 li 맨뒤로 보내기 + left값 0만들기
-        if(lpos < -250){ 
-
-          // 3-1.한계값 초기화!
-          lpos = 0;
-
-          // 3-2.첫번째 li 맨뒤로 보내기
-          flist
-          .append(flist.find("li").first())
-          .css({left:"0"});
-
-        } /////////// if문 /////////////
-        
-        // 4. 재귀호출하기 ///////////
-        setTimeout(moveList, 40);
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    speed: 500,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+      // disableOnInteraction 
+      // - 건드린 후 멈춤 true / 다시 작동은 false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  }); //////// swiper ///////////////
 
 
-      }; //////// moveList 함수 /////////////
+  /// 스크롤 등장 플러그인 적용 : 스크롤리빌 ////
+  $.fn.scrollReveal();
 
-      // 이동함수 최초호출
-      moveList();
+  ////////////////////////////////////////////////
+  //////////////// 신상품 움직이기 ////////////////
+  ////////////////////////////////////////////////
+  // 움직일 대상: .flist
+  let flist = $(".flist");
+  // 위치값변수
+  let lpos = 0;
+  // 재귀호출상태값 변수
+  let cbsts = 1; // 1-호출가능, 0-불가능
+
+  // 신상품 움직이기 함수 ////////////////
+  let moveList = () => {
+
+    // 1. 위치값 감소하기 /////
+    lpos--;
+    // console.log("위치값:"+lpos);
+
+    // 2. 위치값 적용하기 //////
+    flist.css({
+      left: lpos + "px"
+    }); ////// css ////////////
+
+    // 3. 한계값 초기화하기 
+    //    + 첫번째 li 맨뒤로 보내기 + left값 0만들기
+    if (lpos < -300) {
+
+      // 3-1.한계값 초기화!
+      lpos = 0;
+
+      // 3-2.첫번째 li 맨뒤로 보내기
+      flist
+        .append(flist.find("li").first())
+        .css({
+          left: "0"
+        }); //// css ///
+
+    } /////////// if문 /////////////
+
+    // 4. 재귀호출하기 ///////////
+    if (cbsts) setTimeout(moveList, 40);
+
+
+  }; //////// moveList 함수 /////////////
+
+  // 이동함수 최초호출
+  moveList();
+
+  // .flowbx에 마우스 오버시 멈추기/ 아웃시 다시작동하기
+  $(".flowbx").hover(
+    function () { // over
+      cbsts = 0; // 콜백상태값 0
+    },
+    function () { // out
+      cbsts = 1; // 콜백상태값 1
+      moveList(); //함수 재호출!
+    }); /////////// hover /////////
 
 
 }); ///////////// jQB ////////////////////////////
