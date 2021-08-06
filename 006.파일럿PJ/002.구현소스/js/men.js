05e87d19f25af4122fa8307622b7789f2461ed47
Tom <tombap8@hotmail.com>
2021-08-06 13:19:25 +0900

    ss

--- 006.파일럿PJ/002.구현소스/js/men.js
+++ 006.파일럿PJ/002.구현소스/js/men.js
@@ -1,5 +1,8 @@
 // 남자패션페이지 JS - men.js
 
+// 페이지번호(컨텐츠의 위치를 페이지로 생각함!)
+let pno = 0;
+
 // 남성 신상품 정보
 let sinsang = {
   "m1": "[남성]카모전판프린트 PQ 티셔츠^DMTS7K731-MG^99,000원",
@@ -14,6 +17,59 @@ let sinsang = {
 };
 
 $(function () { /// jQB ////////////////////////////
+
+  // 메뉴 a요소 기본이동 막기!
+  $(".gnb a,.indic a").click(function (e) {
+    e.preventDefault();
+}); ///////////// click ////////////////
+
+//// GNB메뉴 클릭시 해당 페이지 위치로 이동 애니메이션
+// 이벤트 대상: .gnb li + .indic li
+// 변경 대상: html,body
+$(".gnb li,.indic li").click(function (e) {
+
+   // 0. 클릭된 li순번 구해오기
+   let idx = $(this).index();
+   //console.log("순번:" + idx);
+
+    // 1. 하위a요소의 href값 읽어오기
+    let idnm = $("a", this).attr("href");
+    //console.log("href값:" + idnm);
+
+    // 2. 아이디값에 해당하위 top값 위치구하기
+    // top값을 구해서 스크롤 애니메이션 이동에 사용함!
+    let pos = $(idnm).offset().top;
+    // offset() 메서드 : 요소의 기본 셋팅 정보를 리턴함
+    // - top,left,width,height 등 을 알수 있다!
+
+    console.log("위치값:" + pos);
+
+
+    // 3. 스크롤 애니메이션으로 페이지 이동하기
+    // 세로스크롤 이동속성: scrollTop
+    // 참고: 가로스크롤 이동속성: scrollLeft
+    // 스크롤 이동대상: html,body
+    // -> 범용브라우저에서 사용하는 스크롤대상
+    $("html,body").animate({
+        scrollTop: pos + "px"
+    }, 1200, "easeOutQuint"); //// animate /////
+
+    
+    // 4. 클릭된 li요소에 class="on" 넣기
+    $(".gnb li").eq(idx).addClass("on")
+    .siblings().removeClass("on");
+    $(".indic li").eq(idx).addClass("on")
+    .siblings().removeClass("on");
+    //다른 형제 li들 class="on" 지움
+    
+    // 6. li순번과 pno와 일치하기! /////////////////////
+    pno = idx;
+    //console.log("페이지번호:" + pno);
+    
+
+}); ///////////// click ///////////////
+
+
 
   /// 부드러운 스크롤 호출!(제이쿼리 아님!)
   startSS();
